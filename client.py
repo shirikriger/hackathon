6984744358878851e5b461fcabb9f4810d7e9f9b
shirikriger <116174730+shirikriger@users.noreply.github.com>
2022-10-26 01:00:54 -0700

    changes

--- client.py
+++ client.py
@@ -1,18 +1,21 @@
 import tkinter as tk
 from tkinter import *
 
-window = Tk()
-
-window.geometry("1000x500")
-
-window.title("choose restaurat")
-
 
 def click(chosen_item):
     return chosen_item
 
 
+item_list = ["1", "2", "3"]
+
+
 def choose_from(item_list):
+    window = Tk()
+
+    window.geometry("1000x500")
+
+    window.title("choose restaurat")
+
     for item in item_list:
         b = Button(window, text=item, font=("Ariel", 10), height=1, width=1000, command=lambda: click(item))
         b.pack()
@@ -20,6 +23,10 @@ def choose_from(item_list):
     finish_frame.pack()
     btn = Button(finish_frame, text="finish", font=("Ariel", 10), height=1, width=10, command=window.destroy)
     btn.pack()
+    window.mainloop()
 
 
-window.mainloop()
+choose_from(item_list)
+
+
+
