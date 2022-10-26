fb566d6baf1b10eaca1bb269445a6333fc765d28
shirikriger <116174730+shirikriger@users.noreply.github.com>
2022-10-26 05:07:38 -0700

    Add files via upload

--- restaurant.py
+++ restaurant.py
@@ -1,1 +1,20 @@
-print("hello world")
+import tkinter as tk
+from tkinter import *
+
+import gui
+
+window = Tk()
+
+window.geometry("1000x500")
+
+window.configure(bg="pink")
+
+window.title("orders")
+
+
+def order(dish):
+    btn = Button(window, text=dish, font=("Ariel", 10), bg="pink", height=1, width=50, command=lambda: btn.pack_forget())
+    btn.pack()
+
+
+window.mainloop()
