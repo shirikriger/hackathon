c82374c3293830c392fddc533d2dac462e4243cb
Shiri Kriger <shiri.kriger@gmail.com>
2022-10-26 10:54:36 -0700

    choosing item

--- client.py
+++ client.py
@@ -1,1 +1,25 @@
-print("hello world")
+import tkinter as tk
+from tkinter import *
+
+window = Tk()
+
+window.geometry("1000x500")
+
+window.title("choose restaurat")
+
+
+def click(chosen_item):
+    return chosen_item
+
+
+def choose_from(item_list):
+    for item in item_list:
+        b = Button(window, text=item, font=("Ariel", 10), height=1, width=1000, command=lambda: click(item))
+        b.pack()
+    finish_frame = Frame(window)
+    finish_frame.pack()
+    btn = Button(finish_frame, text="finish", font=("Ariel", 10), height=1, width=10, command=window.destroy)
+    btn.pack()
+
+
+window.mainloop()
